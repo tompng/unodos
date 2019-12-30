a7157cb7b6164424cab28766cefad7e010c8e3ee
tompng <tomoyapenguin@gmail.com>
2019-12-31 02:59:08 +0900

    add simplecov

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
 # Specify your gem's dependencies in unodos.gemspec
 gemspec
 
-gem "rake", "~> 12.0"
-gem "minitest", "~> 5.0"
+gem 'rake', '~> 12.0'
+gem 'minitest', '~> 5.0'
+gem 'simplecov'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,6 @@
-$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-require "unodos"
+require 'simplecov'
+SimpleCov.start
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+require 'unodos'
 
-require "minitest/autorun"
+require 'minitest/autorun'
